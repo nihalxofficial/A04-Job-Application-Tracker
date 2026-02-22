942ada0b7bd2c27d4496d99d8ba49b2152c490bb
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-02-22 11:55:25 +0600

    Button toggling compoleted

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
-function toggle(){
-    const allFilterBtn = document.getElementById("elementId")
+function toggle(id){
+    const allFilterBtn = document.getElementById("all-filter-btn");
+    const interViewFilterBtn = document.getElementById("interview-filter-btn");
+    const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+    allFilterBtn.classList.remove("btn-primary");
+    interViewFilterBtn.classList.remove("btn-primary");
+    rejectedFilterBtn.classList.remove("btn-primary");
+
+
+    const selected = document.getElementById(id);
+    // console.log(selected);
+    
+    selected.classList.add('btn-primary');
+
 }
