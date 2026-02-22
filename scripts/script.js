3467afdfc82ad59bdbb38aad5288a06ec80eeb5f
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-02-22 13:39:24 +0600

    Rejected filter functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,10 @@ function toggle(id){
         allCardSection.classList.add("hidden");
         filterSection.classList.remove("hidden");
         renderInterView();
+    }else if(id==="rejected-filter-btn"){
+        allCardSection.classList.add("hidden");
+        filterSection.classList.remove("hidden");
+        renderRejected();
     }
 
 }
@@ -26,7 +30,10 @@ function toggle(id){
 
 allCardSection.addEventListener("click", function(event){
     if(event.target.classList.contains("interview-btn")){
-        const parentNode = event.target.parentElement.parentElement.parentElement;
+        let parentNode = event.target.parentElement.parentElement.parentElement;
+        parentNode.classList.remove("border-gray-200");
+        parentNode.classList.add("border-green-400");
+        
 
         const jobCompany = parentNode.querySelector(".job-companies").innerText;
         const jobTitle = parentNode.querySelector(".job-title").innerText;
@@ -34,7 +41,10 @@ allCardSection.addEventListener("click", function(event){
         const jobDescription = parentNode.querySelector(".job-description").innerText;
         let jobStatus = parentNode.querySelector(".job-status").innerText;
         
-        jobStatus = "Interview";
+        jobStatus =  parentNode.querySelector(".job-status").innerText = "INTERVIEW";
+        parentNode.querySelector(".job-status").classList.remove("bg-[#EEF4FF]");
+        parentNode.querySelector(".job-status").classList.add("bg-green-200");
+        
 
         const job = {
             jobCompany,
@@ -52,6 +62,39 @@ allCardSection.addEventListener("click", function(event){
         rejectedList = rejectedList.filter(item => item.jobTitle !== job.jobTitle);
         calculateCount();
         
+    }else if(event.target.classList.contains("rejected-btn")){
+        let parentNode = event.target.parentElement.parentElement.parentElement;
+        parentNode.classList.remove("border-gray-200");
+        parentNode.classList.add("border-red-400");
+        
+
+        const jobCompany = parentNode.querySelector(".job-companies").innerText;
+        const jobTitle = parentNode.querySelector(".job-title").innerText;
+        const jobInfo = parentNode.querySelector(".job-info").innerText;
+        const jobDescription = parentNode.querySelector(".job-description").innerText;
+        let jobStatus = parentNode.querySelector(".job-status").innerText;
+        
+        jobStatus =  parentNode.querySelector(".job-status").innerText = "REJECTED";
+        parentNode.querySelector(".job-status").classList.remove("bg-[#EEF4FF]");
+        parentNode.querySelector(".job-status").classList.add("bg-red-200");
+        
+
+        const job = {
+            jobCompany,
+            jobTitle,
+            jobInfo,
+            jobStatus,
+            jobDescription
+        }
+
+        const jobExist = rejectedList.find(item => item.jobTitle===job.jobTitle);
+        if(!jobExist){
+            rejectedList.push(job);            
+        }
+
+        interViewList = interViewList.filter(item => item.jobTitle !== job.jobTitle);
+        calculateCount();
+        
     }
 })
 
@@ -59,7 +102,33 @@ function renderInterView(){
     filterSection.innerHTML = "";
     for(let list of interViewList){
         const div = document.createElement("div");
-        div.className = "job-card p-7 bg-white rounded-md border border-gray-200 shadow-sm hover:shadow-md flex justify-between";
+        div.className = "job-card p-7 bg-white rounded-md border border-green-400 shadow-sm hover:shadow-md flex justify-between border-l-6";
+        div.innerHTML = `
+        <div class="card-left">
+                <h2 class="job-companies mb-1 font-semibold text-lg text-[#002C5C]">${list.jobCompany}</h2>
+                <p class="job-title text-[#64748B]">${list.jobTitle}</p>
+                <p class="job-info my-5 text-[#64748B]">${list.jobInfo}</p>
+                <h2 class="job-status text-[#002C5C] bg-[#EEF4FF] inline-block px-3 py-2 font-semibold text-sm rounded">${list.jobStatus}</h2>
+                <p class="job-description mt-2 mb-5">${list.jobDescription}</p>
+
+                <div class="job-card-buttons flex gap-3">
+                    <button class="interview-btn btn btn-soft btn-success border-2 border-green-400">Interview</button>
+                    <button class="rejected-btn btn btn-soft btn-error border-2 border-red-400">Rejected</button>
+                </div>
+            </div>
+            <div class="card-right w-8 h-8 border border-gray-200 rounded-full cursor-pointer flex justify-center items-center shrink-0">
+                <img src="./assets/images/Trash.png" alt="" class="">
+            </div>
+        </div>
+        `
+        filterSection.appendChild(div);
+    }
+}
+function renderRejected(){
+    filterSection.innerHTML = "";
+    for(let list of rejectedList){
+        const div = document.createElement("div");
+        div.className = "job-card p-7 bg-white rounded-md border border-red-400 shadow-sm hover:shadow-md flex justify-between border-l-6";
         div.innerHTML = `
         <div class="card-left">
                 <h2 class="job-companies mb-1 font-semibold text-lg text-[#002C5C]">${list.jobCompany}</h2>
